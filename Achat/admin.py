b80d0921c1b6d07f0404f550a790cded20e564c7
Smorta <71671585+Smorta@users.noreply.github.com>
2021-06-24 19:40:36 +0200

    Add files via upload

--- Achat/admin.py
+++ Achat/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from . import models
+from Achat.models import Responsable,Phase,User,Chantier,Modification
 # Register your models here.
-admin.site.register(models.Responsable)
-admin.site.register(models.Phase)
-admin.site.register(models.User)
-admin.site.register(models.Chantier)
-admin.site.register(models.Modification)
+admin.site.register(Responsable)
+admin.site.register(Phase)
+admin.site.register(User)
+admin.site.register(Chantier)
+admin.site.register(Modification)
