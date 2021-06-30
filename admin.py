2da7b4ec97478a2eb627a01288b0826502c86a2c
Smorta <71671585+Smorta@users.noreply.github.com>
2021-06-30 12:26:18 +0200

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from Architect.models import Responsable,Phase,User,Chantier,Modification
-# Register your models here.
-admin.site.register(Responsable)
-admin.site.register(Phase)
-admin.site.register(User)
-admin.site.register(Chantier)
-admin.site.register(Modification)
